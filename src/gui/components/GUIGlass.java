0f3d7c6026e709231c376ac66fa0c549eb910391
Poojan Jhaveri <poojanjhaveri@hotmail.com>
2013-04-27 19:02:17 -0700

    merge conflict in GUIGlass

--- src/gui/components/GUIGlass.java
+++ src/gui/components/GUIGlass.java
@@ -238,14 +238,7 @@ public class GUIGlass extends GuiComponent implements Serializable
 		case WASHER:
 			imageWASHER.display = true;
 			break;
-<<<<<<< HEAD
-=======
-		case NONE1:
-			imageMISSING.display=true;
-			break;	
-			
-			
->>>>>>> updateee donee !
+		
 		}
 	}
 	/**
@@ -325,14 +318,9 @@ public class GUIGlass extends GuiComponent implements Serializable
 		{
 			g2d.drawImage(imageWASHER.holderImage.getImage(), 0, 0, getIcon().getIconWidth(), getIcon().getIconHeight(), this);
 		}
-<<<<<<< HEAD
-=======
-		if (imageMISSING.display)
-		{
-			g2d.drawImage(imageMISSING.holderImage.getImage(), 0, 0, getIcon().getIconWidth(), getIcon().getIconHeight(), this);
-		}
+
 		
->>>>>>> updateee donee !
+
 	}
 
 	/**
